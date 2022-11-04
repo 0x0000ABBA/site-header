2075381bd4ce9f31b575371406f1cf7af8e155ed
seregavspb <96838602+seregavspb@users.noreply.github.com>
2022-11-05 02:50:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const left = document.getElementById("left__side");
+
+console.log(left);
+
+const handleOnMove = e => {
+    const leftWidth = e.clientX / window.innerWidth * 100;
+
+    left.style.width = `${leftWidth}%`;
+}
+
+document.onmousemove = e => handleOnMove(e);
+document.ontouchmove = e => handleOnMove(e.touches[0]);
